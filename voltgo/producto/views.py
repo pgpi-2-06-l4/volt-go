f77678ee28b3a447971b12b53f2c3dc925959b00
3ralon <eralon.dev@gmail.com>
2023-11-19 21:54:08 +0100

    feat(producto): vista detalle base de producto #5

--- voltgo/producto/views.py
+++ voltgo/producto/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from .models import Producto
 
-# Create your views here.
+class ProductDetailView(DetailView):
+    model = Producto
+    template_name = 'detail.html'
+
