1ce60ac0e7401c36347f0743cfe9cbf8e724d462
Francisco Javier Barba <barbatrejofco@gmail.com>
2023-11-20 20:08:41 +0100

    fix: Problema con las vistas de login
    Existía un problema con las vistas definidas en el login y se ha solucionado

--- voltgo/usuario/views.py
+++ voltgo/usuario/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from .models import Usuario
@@ -19,7 +20,10 @@ def user_login(request):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return redirect('usuario:dashboard')
+                    print('login')
+                    dashboard_url = reverse('dashboard') 
+                    print(dashboard_url)
+                    return HttpResponseRedirect(dashboard_url)
                 else:
                     return HttpResponse('Disabled account')
             else:
@@ -31,6 +35,7 @@ def user_login(request):
 @login_required
 def dashboard(request):
     return render(request,
+
                   'account/dashboard.html',
                   {'section': 'dashboard'})
 
@@ -38,14 +43,10 @@ def register(request):
     if request.method == 'POST':
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            # Create a new user object but avoid saving it yet
             new_user = user_form.save(commit=False)
-            # Set the chosen password
             new_user.set_password(
                 user_form.cleaned_data['password'])
-            # Save the User object
             new_user.save()
-            # Create the user profile
             Usuario.objects.create(user=new_user)
             return render(request,
                           'account/register_done.html',
@@ -54,27 +55,25 @@ def register(request):
         user_form = UserRegistrationForm()
     return render(request, 'account/register.html', {'user_form': user_form})
 
-@login_required
-def edit(request):
-    if request.method == 'POST':
-        user_form = UserEditForm(instance=request.user,
-                                 data=request.POST)
-        profile_form = ProfileEditForm(
-            instance=request.user.profile,
-            data=request.POST,
-            files=request.FILES)
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
-            messages.success(request, 'Profile updated '\
-                             'successfully')
-        else:
-            messages.error(request, 'Error updating your profile')
-
-    else:
-        user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(
-            instance=request.user.profile)
-    return render(request, 'account/edit.html', {'user_form': user_form,
-                                                 'profile_form': profile_form})
+# @login_required
+# def edit(request):
+#     if request.method == 'POST':
+#         user_form = UserEditForm(instance=request.user.usuario,
+#                                  data=request.POST)
+#         profile_form = ProfileEditForm(
+#             instance=request.user.usuario,
+#             data=request.POST,
+#             files=request.FILES)
+#         if user_form.is_valid() and profile_form.is_valid():
+#             user_form.save()
+#             profile_form.save()
+#             messages.success(request, 'Profile updated successfully')
+#         else:
+#             messages.error(request, 'Error updating your profile')
+#     else:
+#         user_form = UserEditForm(instance=request.user.usuario)
+#         profile_form = ProfileEditForm(
+#             instance=request.user.usuario)
+#     return render(request, 'account/edit.html', {'user_form': user_form,
+#                                                  'profile_form': profile_form})
 
