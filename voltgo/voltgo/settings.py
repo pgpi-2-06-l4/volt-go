82ec4aac96dd7c24346f7f977d71772a340b9916
Francisco Javier Barba <barbatrejofco@gmail.com>
2023-11-20 08:49:56 +0100

    feat: Creando modelo de Usuario y las vistas principales
    He creado el modelo Usuario y la configuración de este a la vez que se han implementado las vistas asociadas a este módulo.

--- voltgo/voltgo/settings.py
+++ voltgo/voltgo/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'usuario.apps.UsuarioConfig',
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
