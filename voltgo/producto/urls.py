052f08d6759494c3d39bc2522e8fbc049cc329c1
Antonio23337 <antoniobarea233@gmail.com>
2023-11-22 17:36:57 +0100

    feat(catalogo): añadido el catalogo de las motos

--- voltgo/producto/urls.py
+++ voltgo/producto/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ProductDetailView
+from .views import ProductDetailView, catalogo
 
 urlpatterns = [
+    path('catalogo/', catalogo, name='catalogo'),
     path('<pk>/', ProductDetailView.as_view(), name='detalle-producto')
+    
 ]
--- voltgo/producto/views.py
+++ voltgo/producto/views.py
@@ -1,8 +1,30 @@
 from typing import Any
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from .models import Producto
+from django.shortcuts import render
 
 
 class ProductDetailView(DetailView):
     model = Producto
     template_name = "detalle.html"
+
+def catalogo(request):
+    productos = Producto.objects.all()
+    productos_con_caracteristicas = []
+    for producto in productos:
+        autonomia = producto.caracteristicas.filter(nombre='AT').first()
+        velocidad_maxima = producto.caracteristicas.filter(nombre='VM').first()
+
+        autonomia_valor = autonomia.valor if autonomia else 'No disponible'
+        velocidad_maxima_valor = velocidad_maxima.valor if velocidad_maxima else 'No disponible'
+        
+
+        producto_dict = {
+            'producto': producto,
+            'autonomia': autonomia_valor,
+            'velocidad_maxima': velocidad_maxima_valor,
+        }
+        productos_con_caracteristicas.append(producto_dict)
+    
+    return render(request, 'catalogo.html', {'productos': productos_con_caracteristicas})
