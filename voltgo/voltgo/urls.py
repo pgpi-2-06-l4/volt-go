bc5487f90350570b50f955e8f7fd19956204f4e0
Diego Gonzalez Quintanilla <digonquin@gmail.com>
2023-11-23 10:09:55 +0100

    feat(tienda): Implementar escaparate de la aplicacion
    He implementado el escaparate de la aplicacion (home) y creado una
    barra de navegacion comun
    
    Closes #2

--- voltgo/voltgo/urls.py
+++ voltgo/voltgo/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from tienda import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home', views.home_view, name='home'),
 ]
