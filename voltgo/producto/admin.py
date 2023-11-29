e8618def30f6cd381ee6f550da4ff726c004ca14
virolialo <virolialo@gmail.com>
2023-11-29 20:39:33 +0100

    feat(cesta): implementa cesta

--- voltgo/producto/admin.py
+++ voltgo/producto/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from producto.models import Empresa, Caracteristica, Producto
+from producto.models import Empresa, Caracteristica, Producto, ItemCarrito
 
 admin.site.register(Empresa)
 admin.site.register(Caracteristica)
 admin.site.register(Producto)
+admin.site.register(ItemCarrito)
