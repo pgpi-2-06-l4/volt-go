42200e6ccb7cc0c275862b09457da847bfc96d26
Francisco Javier Barba <barbatrejofco@gmail.com>
2023-12-09 12:29:29 +0100

    deploy(tienda): Despliegue continuo completado

--- voltgo/build.sh
+++ voltgo/build.sh
@@ -10,5 +10,3 @@ cd voltgo
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('adm','barbatrejofco@gmail.com','$PASSWORD_ADMIN')" | python manage.py shell
